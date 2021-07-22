056027181c47df02a77b4b6164da749ede76df4a
Diego Ordóñez <diego.ordonez@gmail.com>
2021-07-22 12:49:34 -0500

    Initial

---
+++ src/main/java/dordonez/servers/fotos_rest_srv/RestServerFotosApplication.java
@@ -0,0 +1,13 @@
+package dordonez.servers.fotos_rest_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServerFotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServerFotosApplication.class, args);
+	}
+
+}
